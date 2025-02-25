e6324aec584e702694c10964cdd8b7937bed22b4
Luciana Tudela <luciana.tudela@hotmail.com>
2025-02-25 15:35:25 +0100

    validazione registrazione

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,11 +22,39 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input,
          [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'min:2', 'alpha'],
+            'surname'=> ['required', 'alpha'],
             'email' => ['required','string','email','max:255',
                 Rule::unique(User::class),],
             'password' => $this->passwordRules(),
-        ])->validate();
+            'date'=> ['required', 'date', 'before:' . now()->subYears(18)->toDateString()],
+            'telephone_number'=> ['required', 'regex:/^+?[0-9]{10,15}$/'],
+         ],
+         [
+            'name.required' => 'Il nome è obbligatorio.',
+        'name.alpha' => 'Il nome può contenere solo lettere.',
+        'name.min' => 'Il nome deve contenere almeno 2 caratteri.',
+        
+        'surname.required' => 'Il cognome è obbligatorio.',
+        'surname.alpha' => 'Il cognome può contenere solo lettere.',
+
+        'telephone_number.required' => 'Il numero di telefono è obbligatorio.',
+        'telephone_number.regex' => 'Il numero di telefono dev\'essere nel formato corretto e deve avere tra 8 e 15 cifre.',
+
+        'email.required' => "L'email è obbligatoria.",
+        'email.email' => "Inserisci un'email valida.",
+        'email.unique' => "L'email è già registrata.",
+
+        'password.required' => 'La password è obbligatoria.',
+        'password.min' => 'La password deve contenere almeno 8 caratteri.',
+        'password.confirmed' => 'Le password non coincidono.',
+
+        'date.required' => 'La data di nascita è obbligatoria.',
+        'date.date' => 'Inserisci una data di nascita valida.',
+        'date.before' => 'Devi avere almeno 18 anni per registrarti.',
+         ]
+         )->validate();
+            
 
         return User::create([
             'name' => $input['name'],
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+  <x-error></x-error>
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-6">
