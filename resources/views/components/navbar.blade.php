55d95b47cde78ec331ef38b5a72db05e51fb920a
Luciana Tudela <luciana.tudela@hotmail.com>
2025-03-04 19:32:42 +0100

    /

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -115,6 +115,10 @@
 
                 @endauth
 
+                <x-_locale lang="it" />
+                <x-_locale lang="en" />
+                <x-_locale lang="es" />
+
             </ul>
         </div>
     </div>
