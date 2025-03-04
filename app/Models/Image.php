0f96a677f269c8092ee4cb511d18ced84b675ce6
Luciana Tudela <luciana.tudela@hotmail.com>
2025-03-04 12:11:47 +0100

    user story 5 completata

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use App\Models\Ad;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 {
-    Use HasFactory;
+    use HasFactory;
 
     protected $fillable = [
         'path'
@@ -17,5 +19,21 @@ class Image extends Model
     public function ad(): BelongsTo
     {
         return $this->belongsTo(Ad::class);
+    }
+
+    public static function getUrlByFilePath($filePath, $w = null, $h = null)
+    {
+        if (!$w && !$h) {
+            return Storage::url($filePath);
+        }
+        $path = dirname($filePath);
+        $filename = basename($filePath);
+        $file = "{$path}/crop_{$w}x{$h}_{$filename}";
+        return Storage::url($file);
+    }
+
+    public function getUrl($w = null, $h = null)
+    {
+        return self::getUrlByFilePath($this->path, $w, $h);
     }
 }
