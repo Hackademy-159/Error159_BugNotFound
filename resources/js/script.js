1784ea8846a195779ca8bb5381ff5c4aa03e2878
Gianvito Lovecchio <gianvito1996@gmail.com>
2025-03-04 19:36:17 +0100

    Modifiche alla nav bar nel bottone di ricerca

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,20 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
     let navbar = document.querySelector("#navbar");
     let navElements = document.querySelectorAll(".navElement");
     let header = document.querySelector("#header");
+    let cercaInput = document.querySelector("#cercaInput");
 
     window.addEventListener("scroll", function () {
+        console.log(window.scrollY);
         if (window.scrollY > (header.clientHeight -10)) {
             navElements.forEach(navElements => {
+                //aggiuinge alle scritte colore blu
                 navElements.classList.add("col-b-text");
+                //toglie alle scrittecolore bianco
                 navElements.classList.remove("col-bg-text");
             });
+            //aggiunge alla navbar ombreggiatura e colore sfondo bianco
             navbar.classList.add("col-bg","shadow");
+            cercaInput.classList.add("search-border-all");
+            cercaInput.classList.remove("search-border-home");
         } else {
             navElements.forEach(navElements => {
+                //aggiunge colore bianco
                 navElements.classList.add("col-bg-text");
+                //rimuove colore blu
                 navElements.classList.remove("col-b-text");
             });
+            //rimuove ombra e colore sfondo bianco
             navbar.classList.remove("col-bg","shadow");
+            cercaInput.classList.remove("search-border-all");
+            cercaInput.classList.add("search-border-home");
         }
     });
 });
