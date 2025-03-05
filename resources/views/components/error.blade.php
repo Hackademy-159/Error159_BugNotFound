60d2fd087d6d00931bcf1b82024bfcdfd7957aec
Simone Arena <simonearena10@gmail.com>
2025-03-05 18:33:33 +0100

    sistemazioni vari user story7

--- resources/views/components/error.blade.php
+++ resources/views/components/error.blade.php
@@ -1,7 +1,6 @@
 @if (session('error'))
     <div class="container d-flex justify-content-center z-3">
         <div class="alert alert-danger d-flex align-items-center justify-content-between shadow-sm p-3 rounded position-relative col-md-9">
-            <i class="bi bi-x-circle-fill me-2 fs-5"></i> 
             <span class="flex-grow-1 text-danger fw-semibold">{{ session('error') }}</span>
             <button type="button" class="btn-close position-absolute top-50 end-0 translate-middle-y me-2" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
