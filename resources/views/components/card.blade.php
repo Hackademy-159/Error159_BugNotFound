9b6c1676d639f0976edc787eb77d786f3e84da87
Gianvito Lovecchio <gianvito1996@gmail.com>
2025-03-07 10:59:54 +0100

    sistemata pagina index articoli

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,22 +1,24 @@
 
 <div class="card mx-auto ">
-    <div id="carousel{{ $ad->id }}" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            @foreach ($ad->images as $index => $image)
-                <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                    <img src="{{ $image->getUrl(300,300) }}" class="col-bg img-fluid p-2 no-radius" alt="Immagine dell'articolo {{ $ad->title }}">
-                </div>
-            @endforeach
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carousel{{ $ad->id }}" data-bs-slide="prev">
+    {{-- <div id="carousel{{ $ad->id }}" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner"> --}}
+            {{-- @foreach ($ad->images as $index => $image) --}}
+                {{-- <div class="carousel-item me-0 {{ $index === 0 ? 'active' : '' }}"> --}}
+                    {{-- <img src="{{ $image->getUrl(300,300) }}" class="col-bg img-fluid p-2 no-radius" alt="Immagine dell'articolo {{ $ad->title }}"> --}}
+                    <img src="{{ $ad->images->isNotEmpty() ? $ad->images->first()->getUrl(300,300) : 'https://picsum.photos/200' }}" class="col-bg img-fluid p-2 no-radius" alt="Immagine dell'articolo {{ $ad->title }}">
+
+                {{-- </div> --}}
+            {{-- @endforeach --}}
+        {{-- </div> --}}
+        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#carousel{{ $ad->id }}" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">{{__('ui.Precedente')}}</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carousel{{ $ad->id }}" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">{{__('ui.Successivo')}}</span>
-        </button>
-    </div>
+        </button> --}}
+    {{-- </div> --}}
 
     <div class=" px-3 col-bg no-radius">
         <h5 class=" text-start m-0 col-b-text">{{ $ad->title }}</h5>
@@ -26,7 +28,7 @@
         <h6 class="col-b-text text-start">{{__('ui.Prezzo')}}: {{ $ad->price }} €</h6>
 
         <div class="d-flex justify-content-evenly align-items-center my-1 mb-3">
-            <a href="{{ route('ad.show', compact('ad')) }}" class="cst-button-card2 mt-3">{{__('ui.Dettaglio')}}</a>
+            <a href="{{ route('ad.show', compact('ad')) }}" class="text-center cst-button-card2 mt-3">{{__('ui.Dettaglio')}}</a>
         </div>
         
     </div>
